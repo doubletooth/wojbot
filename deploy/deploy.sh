5d69a37f3e7ebb330f4fe1bf46550937060cc80e
Akshay Dongaonkar <akshaydon93@gmail.com>
2019-06-24 21:17:35 -0700

    Tidy up deploy code. Update state
    - Just want log lines to line up

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,25 +16,26 @@ esac
 done
 
 if [[ -n "$UPDATE_LOCAL" ]]; then
-    gcloud compute scp centos:~/slackbot/state/state.json state/state.json
+    # probably dumb, but don't want file printed out during deploy
+    gcloud compute scp --quiet centos:~/slackbot/state/state.json state/state.json >/dev/null
 fi
 
 
 cd ..  # move out of the base level of the project to zip everything up
 
-echo "[local] Compressing project..."
+echo "[local]  Compressing project..."
 COPYFILE_DISABLE=true tar -zcf slackbot.tar.gz \
 --exclude="*.git" \
 --exclude=".idea*" \
 --exclude="*__pycache__/*" \
 --exclude=".DS_Store" slackbot
 
-echo "[local] Deploying project..."
+echo "[local]  Deploying project..."
 
 # probably dumb, but don't want file printed out during deploy
 gcloud compute scp slackbot.tar.gz centos:~ >/dev/null
-gcloud compute scp --quiet slackbot/deploy/deploy_gcloud.sh centos:~ >/dev/null
+gcloud compute scp slackbot/deploy/deploy_gcloud.sh centos:~ >/dev/null
 
 gcloud compute ssh --command="/home/akshay/deploy_gcloud.sh && rm /home/akshay/deploy_gcloud.sh" centos
 rm slackbot.tar.gz
-echo "[local] Complete"
+echo "[local]  Complete"
